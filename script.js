7f221349d3992642ebbb950bbb02f76c55e922c0
Phoenyx <kraegenbrink@sport-thieme.de>
2022-08-09 15:35:15 +0200

    form v1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function burgerToggle() {
   var links = document.getElementById("links");
-  if ((links.style.display = "block")) {
+  if (links.style.display === "block") {
     links.style.display = "none";
   } else {
     links.style.display = "block";
